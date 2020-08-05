63452917788b849b87e98e1416061f59ca51d9ca
Sam Stewart <samuel.stewart@wwt.com>
2020-08-05 12:07:43 -0400

    add data and scheduled times

--- src/auto_generated/import_vehicle_events_to_mongo.py
+++ src/auto_generated/import_vehicle_events_to_mongo.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 import sys
-from google.protobuf.json_format import MessageToJson 
-import gtfs_realtime_pb2, nyct_subway_pb2
+from google.protobuf.json_format import MessageToJson
+import auto_generated.gtfs_realtime_pb2,auto_generated.nyct_subway_pb2
 import json
 import pymongo
-
+import requests
 
 def main():
-	client = pymongo.MongoClient(
-	       "mongodb://localhost:27017/gtfs?retryWrites=true&w=majority")
-	db = client.gtfs
+#	client = pymongo.MongoClient(
+#	       "mongodb://localhost:27017/gtfs?retryWrites=true&w=majority")
+#	db = client.gtfs
+	r = requests.get(f"https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs",
+				 headers={"x-api-key": "kDjAqBL0oj2xaVN9F4AdM4fFwgsSYfRY7D6BjYtx"})
+	gtfs_realtime = auto_generated.gtfs_realtime_pb2.FeedMessage()
 
-	gtfs_realtime = gtfs_realtime_pb2.FeedMessage()
+	gtfs_realtime.ParseFromString(r.content)
+	message_as_json = json.loads(MessageToJson(gtfs_realtime))
+	print(json.dumps(message_as_json, indent=1)	)
 
-	with open(sys.argv[1],"rb") as f:
-		gtfs_realtime.ParseFromString(f.read())
-		message_as_json = json.loads(MessageToJson(gtfs_realtime))
-       
 		# add the header info to each event
-		events = message_as_json['entity']
-		for e in events:
-			if 'vehicle' in e:
-				e['header'] = message_as_json['header'].copy()
-				db.events.insert(e)
+#		events = message_as_json['entity']
+#		for e in events:
+#			if 'vehicle' in e:
+#				e['header'] = message_as_json['header'].copy()
+#				db.events.insert(e)
  
 if __name__ == '__main__':
 	main()
